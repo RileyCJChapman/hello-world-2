fe17c665256007bff37eccb7a7a6e5ac56af4129
chapmc <rileycjchapman2037@gmail.com>
2022-09-12 11:25:07 -0300

    Initial commit

---
+++ hello world 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD 2");
+            Console.ReadKey(true);
+        }
+    }
+}
